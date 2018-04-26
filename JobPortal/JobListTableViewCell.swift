e1724e71d52e1f6e1b6cc27bfdd713249398eab8
Simi <simifoss@gmail.com>
2018-04-25 22:58:56 -0400

    search view and employee screen

---
+++ JobPortal/JobListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobListTableViewCell.swift
+//  JobPortal
+//
+//  Created by Simi Foss on 4/25/18.
+//  Copyright © 2018 Simi Foss. All rights reserved.
+//
+
+import UIKit
+
+class JobListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
